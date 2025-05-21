ef968a036ad63f7ced027f7f98522e0967521837
ErikaPezzini <erikapezzini66@gmail.com>
2025-05-21 08:05:38 +0200

    feat: crud 1

---
+++ src/main/java/com/example/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -0,0 +1,72 @@
+package com.example.spring_la_mia_pizzeria_crud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.spring_la_mia_pizzeria_crud.model.Pizza;
+import com.example.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
+
+import jakarta.validation.Valid;
+
+
+@Controller
+@RequestMapping("/pizze")
+public class PizzaController {
+
+    @Autowired
+    private PizzaRepository pizzaRepository;
+
+    @GetMapping
+    public String index(Model model){
+        List<Pizza> result = pizzaRepository.findAll(); // = ...dati dal db...
+        model.addAttribute("list", result);
+        return "/pizze/index";
+    }
+
+    @GetMapping("/create")
+    public String createForm(Model model) {
+        model.addAttribute("pizza", new Pizza());
+        return "/pizze/create";
+    }
+
+    @PostMapping("/create")
+    public String doCreate(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/pizze/create";
+        }
+        pizzaRepository.save(formPizza);
+        return "redirect:/pizze";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+    Pizza pizza = pizzaRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("ID non valido: " + id));
+    model.addAttribute("pizza", pizza);
+    return "/pizze/edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@PathVariable Integer id, @Valid @ModelAttribute("pizza") Pizza pizza, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/pizze/edit";
+        }
+    pizza.setId(id); // Assicura che venga aggiornato l'elemento corretto
+    pizzaRepository.save(pizza);
+    return "redirect:/pizze";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+    pizzaRepository.deleteById(id);
+    return "redirect:/pizze";
+    }
+}
---
+++ src/main/java/com/example/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -0,0 +1,97 @@
+package com.example.spring_la_mia_pizzeria_crud.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "pizza")
+public class Pizza {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Il nome è obbligatorio")
+    @Size(max = 255, message = "Il nome non può superare i 255 caratteri")
+    @Column(unique=true,length=255,nullable=false)
+    private String nome;
+
+    @NotBlank(message = "La descrizione è obbligatoria")
+    @Size(max = 500, message = "La descrizione non può superare i 500 caratteri")
+    @Column(length=500)
+    private String descrizione;
+
+    @NotBlank(message = "Il link della foto è obbligatorio")
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String foto;
+
+    @NotNull(message = "Il prezzo è obbligatorio")
+    @DecimalMin(value = "0.01", message = "Il prezzo deve essere maggiore di zero")
+    @Column(nullable=false,precision=8,scale=2)
+    private BigDecimal prezzo;
+
+    @NotNull(message = "Disponibilità richiesta")
+    @Column(nullable=false)
+    private Boolean disponibile;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public BigDecimal getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(BigDecimal prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public Boolean getDisponibile() {
+        return disponibile;
+    }
+
+    public void setDisponibile(Boolean disponibile) {
+        this.disponibile = disponibile;
+    }
+
+
+}
